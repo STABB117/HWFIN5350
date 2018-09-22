7c1c8c8af911a57eccf2ac3d15a78dc68cf79690
stabb117 <steven.dabb@aggiemail.usu.edu)git config --global user.email steven.dabb@aggiemail.usu.edu>
2018-09-22 10:31:10 -0600

    binary homework attempt 1

---
+++ binary.py
@@ -0,0 +1,18 @@
+##python guesses the number
+print( "Let's play a guessing game. I bet that if you choose a number between 1 and 100, I can guess it correctly within 7 tries.")
+
+print("I will try to guess your number. To do so, I will need you to tell me if my guess is too high, too low, or If I guessed the number correctly.")
+
+print("These are the responses that I can recognize: 'too high', 'too low', and 'correct'.")
+
+
+print("My first guess is 50")
+
+guesses = 1
+
+response = input()
+
+
+while guesses < 7
+    if response = "too high":
+            
